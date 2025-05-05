5552f88d56d9065d1a741454520cc8fe3d0e81b3
vinceguev <158005470+vinceguev@users.noreply.github.com>
2025-05-05 00:34:22 -0500

    added prework files to repo

---
+++ PreWork/PreWorkTests/PreWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  PreWorkTests.swift
+//  PreWorkTests
+//
+//  Created by Vince Guevarra on 5/4/25.
+//
+
+import Testing
+@testable import PreWork
+
+struct PreWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
